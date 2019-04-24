0d486e2958a96cc4d1d3d2d1abbc82d52f1008f2
Will <williamwhite0@gmail.com>
2019-04-23 19:06:00 -0700

    Initial commit.

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
 
-    print("COMPLETE THE TODOs")
-    
-
-
+    print(events[0]['created_at'])
